c667e433866ba35a3e7f4840e33b4e14af459e0b
scissormarks <scissormarks@gmail.com>
2017-03-21 11:22:10 +0100

    Echo and print

---
+++ php-basics/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>PHP-Basics</title>
+</head>
+
+<body>
+<?php 
+//Start af en php blok
+//echo viser et data output på en skærm
+	echo "<h1>PHP Basics - The Beginning. </h1>";
+	echo ("This was triggered using PHP. ");
+//print fungere på næsten samme måde
+	print "Print works much like Echo. ";
+	print 'Print is a bit slower, but also gives a return value. ';
+	print('However echo has more functionalty. ')
+//slut af en php blok:
+
+
+?>
+</body>
+</html>
